412da59faeb0ea2706ed75c7102d25b72aad3073
ahasanUllah <bulbulahasan@gmail.com>
2022-09-29 12:28:43 +0600

    exercise time implemented

--- src/Activity/Activity.js
+++ src/Activity/Activity.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const Activity = ({ activity }) => {
-   console.log(activity);
+const Activity = ({ activity, timeRecquire, setTimerecquire }) => {
    const { name, image, id, time, description } = activity;
+   const handleAddToList = () => {
+      const prevTime = timeRecquire;
+      const curtime = time;
+      const newTime = prevTime + curtime;
+      setTimerecquire(newTime);
+   };
    return (
       <div>
          <div className="card card-compact bg-base-100 shadow-xl">
@@ -12,9 +17,11 @@ const Activity = ({ activity }) => {
             <div className="card-body">
                <h2 className="card-title">{name}</h2>
                <p>{description}</p>
-               <p className="font-bold">Time: Reacquired:{time}</p>
+               <p className="font-bold">Time: Reacquired: {time}m</p>
                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Add to list</button>
+                  <button className="btn btn-primary" onClick={handleAddToList}>
+                     Add to list
+                  </button>
                </div>
             </div>
          </div>
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Activity from '../Activity/Activity';
 const Home = () => {
    const [activities, setActivities] = useState([]);
+   const [timeRecquire, setTimerecquire] = useState(0);
    useEffect(() => {
       fetch('data.json')
          .then((res) => res.json())
@@ -11,12 +12,48 @@ const Home = () => {
       <div className="grid grid-cols-3 lg:grid-cols-5">
          <div className="m-5 col-span-2 md:grid md:grid-cols-2 gap-4 lg:col-span-4 lg:grid-cols-3">
             {activities.map((activity) => (
-               <Activity activity={activity} />
+               <Activity activity={activity} timeRecquire={timeRecquire} setTimerecquire={setTimerecquire} />
             ))}
          </div>
-         <div className="bg-primary col-span-1  ">
-            <img src="https://xsgames.co/randomusers/avatar.php?g=male" alt="" className="w-12 rounded-full" />
-            <h1 className="text-md font-bold">Ahsan Ullah</h1>
+         <div className="bg-white col-span-1  ">
+            <div className="flex justify-center items-center mt-8">
+               <img src="https://xsgames.co/randomusers/avatar.php?g=male" alt="" className="w-12 rounded-full" />
+               <div className="ml-3">
+                  <h1 className="text-lg font-bold">Ahsan Ullah</h1>
+                  <p className="text-center text-sm">Black Belt</p>
+               </div>
+            </div>
+            <div className="bg-base-100 flex justify-around items-center mt-8 mx-5 p-2 rounded-md">
+               <div>
+                  <h1 className="font-bold text-2xl">75</h1>
+                  <p>weight</p>
+               </div>
+               <div>
+                  <h1 className="font-bold text-2xl">6.5</h1>
+                  <p>Height</p>
+               </div>
+               <div>
+                  <h1 className="font-bold text-2xl">25</h1>
+                  <p>years</p>
+               </div>
+            </div>
+            <h1 className="text-xl font-bold mt-8 mx-5">Add a break</h1>
+            <div className="bg-base-100 flex justify-around items-center mt-8 mx-5 p-4 rounded-md">
+               <button className="bg-white rounded-full p-1">5m</button>
+               <button className="bg-white rounded-full p-1">10m</button>
+               <button className="bg-white rounded-full p-1">20m</button>
+               <button className="bg-white rounded-full p-1">30m</button>
+               <button className="bg-white rounded-full p-1">40m</button>
+            </div>
+            <h1 className="text-xl font-bold mt-8 mx-5">Activity Details</h1>
+            <div className="bg-base-100 flex justify-around items-center mt-8 mx-5 p-4 rounded-md">
+               <h1 className="font-semibold">Exercise Time</h1>
+               <p>{timeRecquire}m</p>
+            </div>
+            <div className="bg-base-100 flex justify-around items-center mt-8 mx-5 p-4 rounded-md">
+               <h1 className="font-semibold">Break Time</h1>
+               <p>200m</p>
+            </div>
          </div>
       </div>
    );
